b58c00f77c361e704ea5fccaa5a77eb71760baae
osxgirl <osxgirl@mac.com>
2022-12-15 08:00:09 -0500

    frontend 👋🏽

---
+++ frontend/src/App.jsx
@@ -0,0 +1,162 @@
+import React, { useEffect, useState } from "react";
+import { ethers } from "ethers";
+import './App.css';
+import abi from "./utils/WavePortal.json";
+
+
+const getEthereumObject = () => window.ethereum;
+
+/*
+ * This function returns the first linked account found.
+ * If there is no account linked, it will return null.
+ */
+
+const findMetaMaskAccount = async () => {
+  try {
+    const ethereum = getEthereumObject();
+
+    /*
+    * First make sure we have access to the Ethereum object.
+    */
+    if (!ethereum) {
+      console.error("Make sure you have Metamask!");
+      return null;
+    }
+
+    console.log("We have the Ethereum object", ethereum);
+    const accounts = await ethereum.request({ method: "eth_accounts" });
+
+    if (accounts.length !== 0) {
+      const account = accounts[0];
+      console.log("Found an authorized account:", account);
+      return account;
+    } else {
+      console.error("No authorized account found");
+      return null;
+    }
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
+
+const App = () => {
+  const [currentAccount, setCurrentAccount] = useState("");
+
+const [waveCount, setWaveCount] = useState(0);
+const [allWaves, setAllWaves] = useState([])
+ 
+/**
+   * Create a variable here that holds the contract address after you deploy!
+   */
+  const contractAddress = "0x3faE0d43cE054b3DCcf329C318e40133653D5272";
+
+ /**
+   * Create a variable here that references the abi content!
+   */
+const contractABI = abi.abi;  
+  
+const connectWallet = async () => {
+    try {
+      const ethereum = getEthereumObject();
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  /*
+   * This runs our function when the page loads.
+   * More technically, when the App component "mounts".
+   */
+
+  /*
+   * The passed callback function will be run when the page loads.
+   * More technically, when the App component "mounts".
+   */
+ useEffect(async () => {
+    const account = await findMetaMaskAccount();
+    if (account !== null) {
+      setCurrentAccount(account);
+    }
+  }, []);
+
+  const wave = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
+
+        let count = await wavePortalContract.getTotalWaves();
+        localStorage.setItem('waveCount', count)// save count to localStorage 
+        setWaveCount(parseInt(count));
+        console.log("Retrieved total wave count...", count.toNumber());
+
+        /*
+        * Execute the actual wave from your smart contract
+        */
+        const waveTxn = await wavePortalContract.wave();
+        console.log("Mining...", waveTxn.hash);
+
+        await waveTxn.wait();
+        console.log("Mined -- ", waveTxn.hash);
+
+        count = await wavePortalContract.getTotalWaves();
+        localStorage.setItem('waveCount', count)// save count to localStorage 
+        setWaveCount(parseInt(count));
+        console.log("Retrieved total wave count...", count.toNumber());
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  
+  // Declare a new state variable, which we'll call "count"
+  const [count, setCount] = useState("");
+  
+  
+  return (
+    <div className="mainContainer">
+      <div className="dataContainer">
+        <div className="header">
+        👋 Hey there!
+        </div>
+
+        <div className="bio">
+        I am heather and I launched the iPhone at the Palo Alto Apple Store so that's pretty cool right? Connect your Ethereum wallet and wave at me!
+        </div>
+
+        <button className="waveButton" onClick={wave}>
+          Wave at Me
+        </button>
+    
+        {/*
+         * If there is no currentAccount render this button
+         */}
+        {!currentAccount && (
+          <button className="waveButton" onClick={connectWallet}>
+            Connect Wallet
+          </button>
+        )}
+            <p>♥ {waveCount} </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
